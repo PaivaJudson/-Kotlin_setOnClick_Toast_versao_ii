b1af0c5e646577e2319431d97907910876bebc61
Judson Paiva <judson.paiva@hotmail.com>
2021-09-23 00:30:46 +0100

    usando extensões para chamar os elementos pelo id

--- app/src/main/java/com/judson/instituto_de_cdigo_um_vi/MainActivity.kt
+++ app/src/main/java/com/judson/instituto_de_cdigo_um_vi/MainActivity.kt
@@ -2,10 +2,23 @@ package com.judson.instituto_de_cdigo_um_vi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_botao.setOnClickListener {
+            var texto: String = edt_texto.text.toString()
+            Toast.makeText(applicationContext, "Olá "+texto+" tudo bem?", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
